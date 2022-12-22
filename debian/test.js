45376dfbb0c8a89f95b17154978c6bd449af61b6
Deepin Developer <deepin-dev@deepin.org>
2022-12-22 14:11:21 +0800

    feat: Init commit
    init node-url-to-options
    
    Log:

---
+++ debian/test.js
@@ -0,0 +1,41 @@
+'use strict';
+const assert = require('assert');
+const semver = require('semver');
+const urlToOptions = require('../');
+
+const {URL} = require('url');
+
+
+
+// Copied from https://github.com/nodejs/node/blob/master/test/parallel/test-whatwg-url-custom-properties.js
+
+const urlObj = new URL('http://user:pass@foo.bar.com:21/aaa/zzz?l=24#test');
+const opts = urlToOptions(urlObj);
+assert.strictEqual(opts instanceof URL, false);
+assert.strictEqual(opts.protocol, 'http:');
+assert.strictEqual(opts.auth, 'user:pass');
+assert.strictEqual(opts.hostname, 'foo.bar.com');
+assert.strictEqual(opts.port, 21);
+assert.strictEqual(opts.path, '/aaa/zzz?l=24');
+assert.strictEqual(opts.pathname, '/aaa/zzz');
+assert.strictEqual(opts.search, '?l=24');
+assert.strictEqual(opts.hash, '#test');
+
+assert.strictEqual(urlToOptions(new URL('http://[::1]:21')).hostname, '::1');
+
+// If a WHATWG URL object is copied, it is possible that the resulting copy
+// contains the Symbols that Node uses for brand checking, but not the data
+// properties, which are getters. Verify that urlToOptions() can handle such
+// a case.
+const copiedUrlObj = Object.assign({}, urlObj);
+const copiedOpts = urlToOptions(copiedUrlObj);
+assert.strictEqual(copiedOpts instanceof URL, false);
+assert.strictEqual(copiedOpts.protocol, undefined);
+assert.strictEqual(copiedOpts.auth, undefined);
+assert.strictEqual(copiedOpts.hostname, undefined);
+(semver.satisfies(process.version, '>= 9') ? assert.strictEqual : Object.is)(copiedOpts.port, NaN);
+assert.strictEqual(copiedOpts.path, '');
+assert.strictEqual(copiedOpts.pathname, undefined);
+assert.strictEqual(copiedOpts.search, undefined);
+assert.strictEqual(copiedOpts.hash, undefined);
+assert.strictEqual(copiedOpts.href, undefined);
